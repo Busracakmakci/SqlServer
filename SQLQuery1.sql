a6ac7d4bd194e25e69426e82736ef919b28f1785
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-10-31 16:14:55 +0300

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+﻿--Select 
+--ANSII standardı
+Select ContactName Adi,City,CompanyName Sirketadi from Customers
+
+Select * from Customers where City= 'London'
+
+-- case insensitive  <> farklı olanları getir.
+Select * from Products where CategoryID=1 or CategoryID=3
+
+Select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products order by ProductName asc --asending   --descending
+
+select count(*) Adet from Products where CategoryID = 2
+
+select CategoryID, count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+-- datalarımın ıcıne bak grupladıgım kategorılerde kac urun oldgunu ver
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+from products inner join Categories
+on Products.CategoryID = Categories.CategoryID
+where Product.UnitPrice>10
+
+
+--DTO Data Transformation object 
+Select * from Products p inner join [Order Details] od
+on p.ProductID = od.ProductID
+
+Select * from Customer c inner join  Orders o on c.Customer = o.CustomerID
+where o.CustomerID is null
---
+++ SqlServer/SQLQuery2.sql
@@ -0,0 +1,41 @@
+﻿
+---Select 
+---ANSII
+
+Select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers
+
+Select * from Customers where City = 'London'  -- tek tırnakla yazılır 
+
+-- case insensitive ---
+Select * from Products where CategoryID=1 or CategoryID=3
+
+select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products order by CategoryID,ProductName
+
+select * from Products order by UnitPrice asc   --- ascending( asc )vermesen de olur 
+
+select * from Products order by UnitPrice desc  
+
+select * from Products where CategoryID=1 order by UnitPrice desc 
+
+select count(*) from Products  ---bana product tablosunda kaç tane ürün var 
+
+select categoryId from Products group by CategoryID 
+
+select Products.ProductID, Products.ProductName
+from Products inner join Categories        
+on Products.CategoryID = Categories.CategoryID ---- koşulları on ile yapıyoruz 
+
+----------DTO data Transformation object
+Select * from Products p inner join [Order Details] od
+on p.ProductID= od.ProductID
+
+ select * from Customers c left join Orders o 
+ on c.CustomerID = o.CustomerID
+ where o.CustomerID is null  --- o data yok 
+
+
+
+
+
